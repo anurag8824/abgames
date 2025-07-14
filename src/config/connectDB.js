6bb9be96665aa89fdee07bd25e868a5302697ba6
anurag8824 <infayoudigital@gmail.com>
2025-07-14 23:52:31 +0530

    first commit

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -15,20 +15,20 @@
 
 const mysql = require('mysql2/promise');
 
-// const connection = mysql.createPool({
-//     host: 'localhost', // or '127.0.0.1' since it's on the same EC2 server
-//     user: 'gameuser',
-//     password: 'Avatior@123',
-//     database: 'gameavitor',
-//     port: 3306
-// });
-
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',  // XAMPP's default root password is blank
-    database: 'goagamesclub',
+    host: 'localhost', // or '127.0.0.1' since it's on the same EC2 server
+    user: 'gameuser',
+    password: 'Avatior@123',
+    database: 'gameavitor',
     port: 3306
 });
+
+// const connection = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',  // XAMPP's default root password is blank
+//     database: 'goagamesclub',
+//     port: 3306
+// });
 
 module.exports = connection;

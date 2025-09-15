02c7384f1ce7f022dc2e77c7f80f7cfa858eb07d
lokeshvaishnav11 <lokeshkumariit@gmail.com>
2025-09-15 08:56:05 +0530

    FGHJ

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -15,12 +15,12 @@ const mysql = require('mysql2/promise');
 
 // const mysql = require('mysql2/promise');
 
-// const connection = mysql.createPool({
-//   host: '127.0.0.1',  // or '127.0.0.1'
-//   user: 'avaitorgame',
-//   password: 'Avatior123',
-//   database: 'gamedb'
-// });
+const connection = mysql.createPool({
+  host: '127.0.0.1',  // or '127.0.0.1'
+  user: 'avaitorgame',
+  password: 'Avatior123',
+  database: 'gamedb'
+});
 
 // const connection = mysql.createPool({
 //     host: 'localhost',
